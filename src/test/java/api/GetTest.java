3b22e0c4af84aea772f3e087a375325f81d8d7de
RSHA <lokhfuhfmb@gmail.com>
2026-07-07 22:43:27 +0300

    first test

---
+++ src/test/java/api/GetTest.java
@@ -0,0 +1,34 @@
+package api;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetTest {
+
+    private final static String TOKEN="-";
+    private final static String SUGGESTION_URL = "https://suggestions.dadata.ru";
+    private final static String ENDPOINT_IPLOCATE_ADDRESS = "/suggestions/api/4_1/rs/iplocate/address";
+    public String temp="46.226.227.21";
+
+    @Test
+    public void testIPLocate(){
+
+        UserData userData = given()
+                .header("Authorization", "Token " + TOKEN)
+                .queryParam("ip", temp)
+                .when()
+                .contentType(ContentType.JSON)
+                .get(SUGGESTION_URL+ENDPOINT_IPLOCATE_ADDRESS)
+                .then().log().all()
+                .extract().body().jsonPath().getObject("location.data", UserData.class);
+
+        System.out.println("Почтовый индекс: " + userData.getPostal_code());
+        System.out.println("Страна: " + userData.getCountry());
+        System.out.println("Город: " + userData.getCity_with_type());
+        System.out.println("Часовой пояс: " + userData.getTimezone());
+
+    }
+
+}
---
+++ src/test/java/api/UserData.java
@@ -0,0 +1,54 @@
+package api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserData {
+    private String postal_code;
+    private String country;
+    private String city_with_type;
+    private String timezone;
+
+    public UserData(String postal_code, String country, String city_with_type, String timezone) {
+        this.postal_code = postal_code;
+        this.country = country;
+        this.city_with_type = city_with_type;
+        this.timezone = timezone;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity_with_type() {
+        return city_with_type;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public UserData(){
+
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCity_with_type(String city_with_type) {
+        this.city_with_type = city_with_type;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+}
